39d00de4d61f0d116d88a9114c92a7cfa767eb40
t4k48n <34435959+t4k48n@users.noreply.github.com>
2021-03-23 19:29:10 +0900

    fibonacci_list()関数のコメントが実際の挙動と異なるので修正．

--- scratch.py
+++ scratch.py
@@ -17,13 +17,8 @@ def fibonacci(n):
 fibonacci.memo = {}
 
 def fibonacci_list(n):
-    """ フィボナッチ数列のn番目を計算する．
-
-    定義:
-        fibonacci(0) = 0
-        fibonacci(1) = 1
-        fibonacci(2) = fibonacci(n - 1) + fibonacci(n - 2)
-        ただし，n >=0
+    """ フィボナッチ数列のn番目までを計算してリストに格納して返す．
+    フィボナッチ数列の定義はfibonacci()関数に準拠．
     """
     return [fibonacci(i) for i in range(0, n + 1)]
 
