02c76a1a9f6139352db71a9319d351a75a91f5be
Takatani, Hideaki <hide.mop@gmail.com>
2021-03-23 18:13:07 +0900

    数列の計算部分を関数として切り出し

--- scratch.py
+++ scratch.py
@@ -1,16 +1,10 @@
-n1 = 0
-n2 = 1
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
 N = 10
 
 # 0からNまでのフィボナッチ数列を表示．
 for i in range(N + 1):
-    if i == 0:
-        print(n1)
-    elif i == 1:
-        print(n2)
-    else:
-        n_ = n1
-        n1 = n2
-        n2 = n2 + n_
-        print(n2)
+    print(fibonacci(i))
