87c055be558adaf041dcf3f4fe72876a2145df3d
Takatani, Hideaki <hide.mop@gmail.com>
2021-03-23 18:21:47 +0900

    フィボナッチ数列のリストを作る関数を実装

--- scratch.py
+++ scratch.py
@@ -3,8 +3,13 @@ def fibonacci(n):
         return n
     return fibonacci(n - 1) + fibonacci(n - 2)
 
+def fibonacci_list(n):
+    return [fibonacci(i) for i in range(0, n + 1)]
+
 N = 10
 
 # 0からNまでのフィボナッチ数列を表示．
 for i in range(N + 1):
     print(fibonacci(i))
+
+print(fibonacci_list(10))
