66c8db6e13b6bb5f78ec967b0fa161e695afaa48
Takatani, Hideaki <hide.mop@gmail.com>
2021-03-23 18:25:09 +0900

    fibonacci関数をメモ化して高速化（？）

--- scratch.py
+++ scratch.py
@@ -1,7 +1,12 @@
 def fibonacci(n):
+    if n in fibonacci.memo:
+        return fibonacci.memo[n]
     if n <= 1:
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+        fibonacci.memo[n] = n
+        return fibonacci.memo[n]
+    fibonacci.memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    return fibonacci.memo[n]
+fibonacci.memo = {}
 
 N = 10
 
